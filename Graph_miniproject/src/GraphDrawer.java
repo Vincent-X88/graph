f9ed338405fb07f4014b81b7476747b050bcd3df
Xolani Mavuso <vxolani777@gmail.com>
2024-04-24 12:48:20 +0200

    Update GraphDrawer.java

--- Graph_miniproject/src/GraphDrawer.java
+++ Graph_miniproject/src/GraphDrawer.java
@@ -1,4 +1,4 @@
-import edu.uci.ics.jung.algorithms.layout.CircleLayout
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
